c39ded1438f6b26ef7205bcc13b38b8ae9c11ed9
Richard Tobing <rlumb001@ucr.edu>
2021-10-20 19:53:36 -0700

    this version uses a small loop instead of a swtich  statement for controlling both the state and the output

---
+++ source/main.c
@@ -0,0 +1,107 @@
+/*	Author: Richard Tobing, rlumb001@ucr.edu
+ *  Partner(s) Name: 
+ *	Lab Section:
+ *	Assignment: Lab #6  Exercise #1
+ *	Exercise Description: [optional - include for your own benefit]
+ *
+ *	I acknowledge all content contained herein, excluding timer.h or example
+ *	code, is my own original work.
+ */
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif
+
+//----------------------------------------timer.h----------------------------------------------//
+volatile unsigned char TimerFlag = 0; 
+unsigned long _avr_timer_M = 1; 
+unsigned long _avr_timer_cntcurr = 0; 
+
+
+void TimerOn() {
+  TCCR1B = 0x0B;
+  OCR1A = 125;
+  TIMSK1 = 0x02;
+  TCNT1 = 0;
+  _avr_timer_cntcurr = _avr_timer_M;
+  SREG |= 0x80;
+}
+
+void TimerOff() {
+  TCCR1B = 0x00;
+}
+
+void TimerISR() {
+  TimerFlag = 1;
+}
+
+ISR(TIMER1_COMPA_vect) {
+	_avr_timer_cntcurr--; 
+	if(_avr_timer_cntcurr == 0) { 
+		TimerISR(); 
+		_avr_timer_cntcurr = _avr_timer_M;
+	}
+}
+
+void TimerSet(unsigned long M) {
+	_avr_timer_M = M;
+	_avr_timer_cntcurr = _avr_timer_M;
+}
+
+////----------------------------------------/timer.h----------------------------------------------////
+
+enum States{state1, state2, state3}state;
+unsigned char out = 0;
+
+
+void Tick(){
+  // switch(state) {
+  //   case state1:
+  //     out = 1;
+  //     //--------------------------------------------------------
+  //     state = state2;
+  //   break;
+
+  //   case state2:
+  //     out = 2;
+  //     //--------------------------------------------------------
+  //     state = state3;
+  //   break;
+
+  //   case state3:
+  //     out = 4;
+  //     //-------------------------------------------------------
+  //     state = state1;
+  //   break;
+  // }
+
+
+
+  if(state == state1){
+      if((out==4) || (out==0)){ out = 1; }
+      else{out = 2*out; }
+  }
+
+}
+
+
+int main(void) {
+    DDRB = 0xFF;  PORTB = 0x00;
+
+    TimerSet(1000);
+    TimerOn();
+
+    state = state1;
+
+
+    while (1) {
+        
+        Tick();
+        while (!TimerFlag); 
+        TimerFlag = 0;
+    PORTB = out; 
+    }
+    //return 1;
+}
+
---
+++ test/tests.py
@@ -0,0 +1,121 @@
+# Array of tests to run (in order)
+# Each test contains
+#   description - 
+#   steps - A list of steps to perform, each step can have
+#       inputs - A list of tuples for the inputs to apply at that step
+#       *time - The time (in ms) to wait before continuing to the next step 
+#           and before checking expected values for this step. The time should be a multiple of
+#           the period of the system
+#       *iterations - The number of clock ticks to wait (periods)
+#       expected - The expected value at the end of this step (after the "time" has elapsed.) 
+#           If this value is incorrect the test will fail early before completing.
+#       * only one of these should be used
+#   expected - The expected output (as a list of tuples) at the end of this test
+# An example set of tests is shown below. It is important to note that these tests are not "unit tests" in 
+# that they are not ran in isolation but in the order shown and the state of the device is not reset or 
+# altered in between executions (unless preconditions are used).
+tests = [ 
+    
+    {
+        'description': 'first iteration',
+        'steps': 
+            [ 
+                
+                {'inputs': [('PINA', 0)],'iterations': 1, 'expected': [('PORTB',1)]}, # Set PIN to val then run one iteration
+       
+            ],
+        'expected': [('PORTB',1)],
+    },
+
+#--------------------------------------------------------------------------------------
+
+    {
+        'description': 'second ',
+        'steps': 
+            [ 
+                
+                {'inputs': [('PINA', 0)],'iterations': 1, 'expected': [('PORTB',2)]}, # Set PIN to val then run one iteration
+       
+            ],
+        'expected': [('PORTB',2)],
+    },
+
+#--------------------------------------------------------------------------------------
+
+{
+        'description': 'third',
+        'steps': 
+            [ 
+                
+                {'inputs': [('PINA', 0)],'iterations': 1, 'expected': [('PORTB',4)]}, # Set PIN to val then run one iteration
+       
+            ],
+        'expected': [('PORTB',4)],
+    },
+
+#--------------------------------------------------------------------------------------
+
+    {
+        'description': 'fourth ',
+        'steps': 
+            [ 
+                
+                {'inputs': [('PINA', 0)],'iterations': 1, 'expected': [('PORTB',1)]}, # Set PIN to val then run one iteration
+       
+            ],
+        'expected': [('PORTB',1)],
+    },
+
+#--------------------------------------------------------------------------------------
+{
+        'description': 'fifth ',
+        'steps': 
+            [ 
+                
+                {'inputs': [('PINA', 0)],'iterations': 1, 'expected': [('PORTB',2)]}, # Set PIN to val then run one iteration
+       
+            ],
+        'expected': [('PORTB',2)],
+    },
+
+#--------------------------------------------------------------------------------------
+
+    
+
+
+
+    {
+        'description': 'its about time. worry about this one later',
+        'steps': 
+            [ 
+                {'inputs': [('PINA',0)], 'time': 300, 'expected': [('PORTB',1)]},
+                {'inputs': [('PINA',0)], 'time': 1000, 'expected': [('PORTB',2)]},
+                {'inputs': [('PINA',0)], 'time': 1000, 'expected': [('PORTB',4)]},
+               # {'inputs': [('PINA',0)], 'time': 1000, 'expected': [('PORTB',1)]},
+               # {'inputs': [('PINA',0)], 'time': 1000, 'expected': [('PORTB',2)]},
+                
+            ],
+        'expected': [('PORTB',2)],
+    },
+
+#--------------------------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+]
+
+watch = ['out','PORTB']
+
+
+# Optionally you can add a set of "watch" variables these need to be global or static and may need
+# to be scoped at the function level (for static variables) if there are naming conflicts. The 
+# variables listed here will display everytime you hit (and stop at) a breakpoint
+#watch = ['<function>::<static-var>','PORTB']
+
