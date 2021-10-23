def6a43f961455204fb41dd856cbc39d817de301
richardTobing <rlumb001@ucr.edu>
2021-10-22 19:08:18 -0700

    lab6 p1 is working with local tests, to to push, then work on vm to test with breadboard

--- source/main.c
+++ source/main.c
@@ -17,8 +17,6 @@
 volatile unsigned char TimerFlag = 0; 
 unsigned long _avr_timer_M = 1; 
 unsigned long _avr_timer_cntcurr = 0; 
-
-
 void TimerOn() {
   TCCR1B = 0x0B;
   OCR1A = 125;
@@ -27,15 +25,8 @@ void TimerOn() {
   _avr_timer_cntcurr = _avr_timer_M;
   SREG |= 0x80;
 }
-
-void TimerOff() {
-  TCCR1B = 0x00;
-}
-
-void TimerISR() {
-  TimerFlag = 1;
-}
-
+void TimerOff() {TCCR1B = 0x00;}
+void TimerISR() {TimerFlag = 1;}
 ISR(TIMER1_COMPA_vect) {
 	_avr_timer_cntcurr--; 
 	if(_avr_timer_cntcurr == 0) { 
@@ -43,12 +34,10 @@ ISR(TIMER1_COMPA_vect) {
 		_avr_timer_cntcurr = _avr_timer_M;
 	}
 }
-
 void TimerSet(unsigned long M) {
 	_avr_timer_M = M;
 	_avr_timer_cntcurr = _avr_timer_M;
 }
-
 ////----------------------------------------/timer.h----------------------------------------------////
 
 enum States{state1, state2, state3}state;
